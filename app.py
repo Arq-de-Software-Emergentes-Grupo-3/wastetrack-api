dcc92bba235f47a62b3b60cc45e8b5a8e50efc82
daniel <u202121440@upc.edu.pe>
2025-05-14 01:52:38 -0500

    ♻️ Refactor (routes): Add user and auth routers

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 from routes.container import container
-from config.db import engine
-from models.container import Base
+from routes.auth import auth
+from routes.user import user
+from config.db import engine, Base
+
+import models.container
+import models.user
 
 app = FastAPI(
     title="Waste Track",
     version="0.0.1",
 )
 
-# Crear todas las tablas registradas
 Base.metadata.create_all(bind=engine)
 
+app.include_router(auth, prefix="/api/v1")
+app.include_router(user, prefix="/api/v1")
 app.include_router(container, prefix="/api/v1")
